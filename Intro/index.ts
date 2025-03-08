275273651cc8227c4e6810e139373eddb94d319e
ajuuz <ajuuz07@gmail.com>
2025-03-08 14:03:17 +0530

    covered some more topic

--- Intro/index.ts
+++ Intro/index.ts
@@ -22,17 +22,54 @@ let userDetail : {name:string ; age:number} = {
 //interface
 interface Details {
     name:string;
-    age:number;
-    salary:number;
+    age:number|string;  // UNION   - we can insert either number or string . just like or operator
+    salary?:number;     //OPTIONAL  - these fields are optional . even if we omit there will be no error
+    getName?:()=>void;
 }
 
 let employeeDetails : Details = {
     name:"employee1",
-    age:20,
-    salary:100000
+    age:"20",
+    getName() {
+        console.log(this.name)
+    },
 }
 let managerDetails : Details = {
     name:"ajmal",
     age:22,
-    salary:1000000
+    salary:1000000,
+    
 }
+
+//type
+type DetailsType = {
+    name:string,
+    age:number | string,
+    salary?:number,
+}
+
+let employeeDetails1 : DetailsType={
+    name:"ajmal",
+    age:22,
+    salary:500000000
+}
+
+
+//how to specify that the array contains multiple types
+let array : (string|number|boolean)[]=[1,2,3,"string1",true]
+
+
+// function
+
+function getUserName(employeeDetails1:DetailsType):string{   //=> here we have to mention string as the return type 
+        return employeeDetails1.name;
+}
+
+console.log(getUserName(employeeDetails1));
+
+
+function getUserDetails(userDetails:{name:string,age:number}){
+            return userDetails.name;
+}
+
+getUserDetails({name:"ajmal",age:22})
